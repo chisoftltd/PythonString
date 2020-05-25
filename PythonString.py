181d571ba076fd0e73cbb0b00f742379dbc95277
chisoftltd <chisoftltd@gmail.com>
2020-05-25 22:07:47 +0200

    # Accessing String Values
    # Special String Operators

---
+++ PythonString.py
@@ -0,0 +1,26 @@
+# Program by ChisoftMedia
+# Accessing String Values
+
+
+channel = "Python Programming Tutorials"
+name = "ChisoftMedia"
+
+str1 = channel[0]
+firstname = name[0:7]
+
+print(str1)
+print(firstname)
+
+print(name + firstname)
+
+print(str1 * 7)
+
+print(name[3])
+
+print("C" in name)
+
+# Special String Operators
+
+print(name + "\r" + firstname)
+print(name + firstname[-5:-2])
+print(name + "\n" + firstname)

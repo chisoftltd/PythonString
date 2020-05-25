81f1cfb793bf524e6a2b49fcc6d966017f6f9601
chisoftltd <chisoftltd@gmail.com>
2020-05-25 22:19:41 +0200

    # Formatting a String
    # Multi-line Strings

--- PythonString.py
+++ PythonString.py
@@ -24,3 +24,21 @@ print("C" in name)
 print(name + "\r" + firstname)
 print(name + firstname[-5:-2])
 print(name + "\n" + firstname)
+
+# Formatting a String
+print("My first car was %s. I used it for %d years " %("Vauxhall Insignia", 2))
+
+
+# Multi-line Strings
+text = """it is very good for you to learn programming. 
+I also think that starting with Python is probably the
+easiest way to achieve it. The remember to watch the 
+videos"""
+
+print(text)
+
+print(text.capitalize())
+
+print(text.center(20))
+
+print(text.count("it"))
